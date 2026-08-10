b23c8ca4a91c1446acb4430761077d14f8c9b758
SVo7777777 <svetl77voronina@yandex.ru>
2026-08-10 17:46:18 +0300

    09.08.2026

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.screenclock"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "2.2"
+        versionCode = 5
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
